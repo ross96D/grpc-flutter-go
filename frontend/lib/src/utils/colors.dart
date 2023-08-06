41716e1ea841d49c8bac6485ee75892f352f0996
ross96D <abelaross96@gmail.com>
2023-08-06 15:20:55 -0400

    feat: added const colors for the app

---
+++ frontend/lib/src/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// Color foregroundColor(Color backgroundColor) {
+//   return backgroundColor.computeLuminance() > 0.5 ? Colors.black12 : Colors.white70;
+// }
+
+const Color foregroundColor = Colors.white70;
+
+const Color textBackgroundColor =Color.fromARGB(255, 6, 46, 73);
+
+const Color scaffoldBackground = Colors.black12;
