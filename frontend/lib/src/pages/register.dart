89a56a7fef9457e8dd3a5c512c779ef43764c6c0
ross96D <abelaross96@gmail.com>
2023-08-06 17:45:21 -0400

    feat: register page

--- frontend/lib/src/pages/register.dart
+++ frontend/lib/src/pages/register.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/src/generated/user.pbgrpc.dart';
+import 'package:frontend/src/statics.dart';
+import 'package:frontend/src/utils/colors.dart';
 
 class PageRegister extends StatefulWidget {
   const PageRegister({Key? key}) : super(key: key);
@@ -8,10 +11,209 @@ class PageRegister extends StatefulWidget {
 }
 
 class _PageRegisterState extends State<PageRegister> {
+  late UserServiceClient _stub;
+  final _formKey = GlobalKey<FormState>();
+  final User user = User();
+  String secondPassword = '';
+
+  @override
+  void initState() {
+    _stub = UserServiceClient(Connection.channel);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('HOLAA'),
+    const textStyle = TextStyle(
+      color: foregroundColor,
     );
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Register'),
+        ),
+        backgroundColor: scaffoldBackground,
+        body: Stack(
+          children: [
+            Center(
+              child: Container(
+                width: 500,
+                color: textBackgroundColor,
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      ListTile(
+                        leading: const Icon(Icons.person),
+                        title: TextFormField(
+                          style: textStyle,
+                          decoration: const InputDecoration(
+                            labelText: 'Name',
+                            labelStyle: textStyle,
+                          ),
+                          onFieldSubmitted: (value) {
+                            setState(() {
+                              user.name = value;
+                            });
+                          },
+                          onChanged: (value) {
+                            setState(() {
+                              user.name = value;
+                            });
+                          },
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.person_outline_rounded),
+                        title: TextFormField(
+                          style: textStyle,
+                          decoration: const InputDecoration(
+                            labelText: 'Lastname',
+                            labelStyle: textStyle,
+                          ),
+                          onFieldSubmitted: (value) {
+                            setState(() {
+                              user.lastname = value;
+                            });
+                          },
+                          onChanged: (value) {
+                            setState(() {
+                              user.lastname = value;
+                            });
+                          },
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.email),
+                        title: TextFormField(
+                          style: textStyle,
+                          decoration: const InputDecoration(
+                            labelText: 'E-Mail',
+                            labelStyle: textStyle,
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          onFieldSubmitted: (value) {
+                            setState(() {
+                              user.email = value;
+                            });
+                          },
+                          onChanged: (value) {
+                            setState(() {
+                              user.email = value;
+                            });
+                          },
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.password),
+                        title: TextFormField(
+                          style: textStyle,
+                          decoration: const InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: textStyle,
+                          ),
+                          obscureText: true,
+                          onFieldSubmitted: (value) {
+                            setState(() {
+                              user.password = value;
+                            });
+                          },
+                          onChanged: (value) {
+                            setState(() {
+                              user.password = value;
+                            });
+                          },
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.repeat),
+                        title: TextFormField(
+                          style: textStyle,
+                          decoration: const InputDecoration(
+                            labelText: 'Repeat Password',
+                            labelStyle: textStyle,
+                          ),
+                          obscureText: true,
+                          onFieldSubmitted: (value) {
+                            setState(() {
+                              secondPassword = value;
+                            });
+                          },
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter some text';
+                            } else if (value != user.password) {
+                              print(value);
+                              print(user.password);
+                              return 'Passwords dont match';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(50)
+                        ),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            register();
+                          }
+                        },
+                        child: const Text('Submit'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+  
+  void register() async {
+    print('AAAAAAA');
+    try {
+      print(user.toString());
+      RegisterResult result = await _stub.register(user);
+      print('BBBBBBB');
+
+    if (result.result) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('User registered sucessfully')));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('User could not be registered')));
+    }
+    } catch (e, st) {
+      print(e);
+      print(st);
+      rethrow;
+    }
+    
   }
 }
