3ff701cafbee90b7f78686de424988172c6fc6bd
ross96D <abelaross96@gmail.com>
2023-08-06 15:23:20 -0400

    feat: non finished register page

---
+++ frontend/lib/src/pages/register.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PageRegister extends StatefulWidget {
+  const PageRegister({Key? key}) : super(key: key);
+
+  @override
+  _PageRegisterState createState() => _PageRegisterState();
+}
+
+class _PageRegisterState extends State<PageRegister> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('HOLAA'),
+    );
+  }
+}
