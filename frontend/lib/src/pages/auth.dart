2ffe7fdb7f0f55390e81fbcc0fea78747845d9c2
ross96D <abelaross96@gmail.com>
2023-08-06 15:22:48 -0400

    feat: added the login page

--- frontend/lib/src/pages/auth.dart
+++ frontend/lib/src/pages/auth.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/src/pages/register.dart';
 import 'package:frontend/src/statics.dart';
 import 'package:frontend/src/generated/user.pbgrpc.dart';
-
+import 'package:frontend/src/utils/colors.dart';
 
 final passwordFocusNode = FocusNode();
 
 class PageAuth extends StatefulWidget {
   const PageAuth({Key? key}) : super(key: key);
-  
+
   @override
   _PageAuthState createState() => _PageAuthState();
 }
@@ -26,69 +27,101 @@ class _PageAuthState extends State<PageAuth> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.cyan,
-      body: Card(
-        color: Colors.purple,
-        child: Padding(
-          padding: EdgeInsets.only(bottom: 24, left: 24, right: 24, top: 16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) => setState(() => email = value),
-                decoration: const InputDecoration(
-                  labelText: "Email",
-                  contentPadding: EdgeInsets.only(
-                    right: 0,
-                    left: 0,
-                    bottom: 8,
-                    top: 12,
+        backgroundColor: scaffoldBackground,
+        body: Stack(
+          children: [
+            Positioned(
+              top: 5.0,
+              right: 5.0,
+              child: TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith<Color>((states) => textBackgroundColor)
+                ),
+                onPressed: register,
+                child: const Text("Registrarte", style: TextStyle(color: foregroundColor)),
+              ),
+            ),
+            Center(
+              child: Card(
+                color: const Color.fromARGB(255, 6, 46, 73),
+                child: Container(
+                  width: 306+48.0,
+                  padding: const EdgeInsets.only(bottom: 24, left: 24, right: 24, top: 16),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        onChanged: (value) => setState(() => email = value),
+                        style: const TextStyle(
+                          color: foregroundColor
+                        ),
+                        decoration: const InputDecoration(
+                          labelStyle: TextStyle(
+                            color: foregroundColor
+                          ),
+                          labelText: "Email",
+                          contentPadding: EdgeInsets.only(
+                            right: 0,
+                            left: 0,
+                            bottom: 8,
+                            top: 12,
+                          ),
+                        ),
+                        onFieldSubmitted: (value) {
+                          if (email != null) {
+                            passwordFocusNode.requestFocus();
+                          }
+                        },
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Stack(
+                        children: [
+                          TextFormField(
+                            focusNode: passwordFocusNode,
+                            obscureText: true,
+                            onChanged: (value) {
+                              setState(() {
+                                password = value;
+                              });
+                            },
+                            style: const TextStyle(
+                              color: foregroundColor
+                            ),
+                            decoration: const InputDecoration(
+                              labelStyle: TextStyle(
+                                color: foregroundColor
+                              ),
+                              labelText: "Contraseña",
+                              contentPadding: EdgeInsets.only(right: 40, left: 0, bottom: 8, top: 12),
+                            ),
+                            onFieldSubmitted: (value) {
+                              if (email != null && password != null) {
+                                login();
+                              }
+                            },
+                          ),
+                        ],
+                      )
+                    ],
                   ),
                 ),
-                onFieldSubmitted: (value) {
-                  if (email != null) {
-                    passwordFocusNode.requestFocus();
-                  }
-                },
               ),
-              const SizedBox(height: 12,),
-              Stack(
-                children: [
-                  TextFormField(
-                    focusNode: passwordFocusNode,
-                    obscureText: true,
-                    onChanged: (value) {
-                      setState(() {
-                        password = value;
-                      });
-                    },
-                    decoration: const InputDecoration(
-                      labelText: "Contraseña",
-                      contentPadding: EdgeInsets.only(right: 40, left: 0, bottom: 8, top: 12),
-                    ),
-                    onFieldSubmitted: (value) {
-                      if (email != null && password != null) {
-                        login();
-                      }
-                    },
-                  ),
-                ],
-              )
-            ],
-            
-            
-          ),
-        ),
-      ),
-    );
+            ),
+          ],
+        ));
   }
-  
+
   void login() async {
     Values.token = await _stub.login(LoginParams(email: email, password: password));
   }
+
+  void register() async {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) => const PageRegister())
+    );
+  }
 }
-
-
-
